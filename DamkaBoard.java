1da88384a90b6728cf57a4af68bae00abe3940c1
alonmorad9 <alon.morad@post.runi.ac.il>
2023-12-28 18:40:41 +0200

    first push

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -7,7 +7,10 @@ public class DamkaBoard {
 		int num = Integer.parseInt(args[0]);
 		for (int i = 0; i < num; i++){
 			for (int j = 0; j < num; j++){
-				System.out.print("* ");
+				if (i % 2 == 0)
+					System.out.print(" *");
+				else
+					System.out.print("* ");
 			}
 			System.out.println();
 		}
